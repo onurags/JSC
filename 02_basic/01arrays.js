3694f9b74898ec25f09371b71c9cd06d1bca6581
Anurag Ganvir <144724285+anuragganvir@users.noreply.github.com>
2024-11-03 10:59:26 +0000

    array01

---
+++ 02_basic/01arrays.js
@@ -0,0 +1,36 @@
+            // array
+
+const myArr = [0, 1, 2, 3, 4, 5]
+const myHeors = ["shaktiman", "naagraj"]
+
+const myArr2 = new Array(1, 2, 3, 4)
+// console.log(myArr[1]);
+
+            //array methods
+
+// myArr.push(6);
+// myArr.push(7);
+// myArr.pop();
+// myArr.pop();
+
+// myArr.unshift(7)
+// myArr.shift()
+
+// console.log(myArr.includes(9));
+// console.log(myArr.indexOf(3));
+
+// const newArr = myArr.join()
+
+// console.log(myArr);
+// console.log( newArr);
+
+            // slice, splice
+
+console.log("A :", myArr);
+
+const myn1 = myArr.slice(1, 3)
+
+console.log("slice : ", myn1);
+
+const myn2 = myArr.splice(1, 3)
+console.log("splice : ", myArr);
