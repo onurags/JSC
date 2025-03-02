836681bca149d64405c89a0943e9e1b2105db5a5
Anurag Ganvir <144724285+anuragganvir@users.noreply.github.com>
2025-03-02 05:49:34 +0000

    arrow

--- 03_basic/03Arrow.js
+++ 03_basic/03Arrow.js
@@ -20,9 +20,13 @@ const user = {
 
 //arrow function
 // const addTwo = (num1, num2) => {
-//     return num1 + num2
+//     return num1 + num2             // explicit return
 // }
 
-const addTwo = (num1, num2) => (num1 + num2) //implicit return
+const addTwo = (num1, num2) => (num1 + num2); //implicit return (no need to write return keyword)
+const addTwos = (num1, num2) =>  { return num1 + num2}
+
+
 
 console.log(addTwo(4, 5));
+console.log(addTwo(3, 5));
