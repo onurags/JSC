33587eb94d6e02e5ad75e548d1588b1b38fc9dba
Anurag Ganvir <144724285+anuragganvir@users.noreply.github.com>
2025-03-02 03:50:45 +0000

    function

--- 03_basic/01functions.js
+++ 03_basic/01functions.js
@@ -30,10 +30,50 @@ const res = addTwoNumbers(4, 8)
 // ------------------------------------
 
 function loginUserMessage(username){
-    if (username === undefined) {
+    if (!username) {
         console.log("Please enter username");
-        return;
-    }
+        
+    } else {
     return `${username} Just logged in`
+    }
 }
-console.log(loginUserMessage());
+// console.log(loginUserMessage("josephroot"));
+
+
+// -----------------------------------------
+
+function calculateCartPrice(val1, val2, ...num1){
+    return  num1;
+}
+
+// console.log(calculateCartPrice(200, 300, 400, 900));
+
+// ----------------------------------
+// Handle object in the function
+
+const user = {
+    username: "kevinpeterson",
+    number: 56,
+}
+
+function handleObject(object){
+    console.log(`Username is ${object.username} and it's number is ${object.number} \n`)
+}
+
+// handleObject(user);
+// handleObject({
+    username:"jackkallis",
+    number: 555,
+})
+
+ //handle array
+
+ const myArray = [200, 500, 633, 423, 530];
+
+ function returnSecondnFifthValue(arr){
+    return [arr[2], arr[4]];
+ }
+
+//  console.log(returnSecondnFifthValue(myArray));
+
+// ---------------------------
