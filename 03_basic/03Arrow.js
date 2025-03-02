0342f83a04d81fd68fc3049db5c63a10d106f37c
Anurag Ganvir <144724285+anuragganvir@users.noreply.github.com>
2025-03-02 05:53:59 +0000

    arrow

--- 03_basic/03Arrow.js
+++ 03_basic/03Arrow.js
@@ -18,7 +18,8 @@ const user = {
 // chai()
 
 
-//arrow function
+                                    //arrow function
+                                    
 // const addTwo = (num1, num2) => {
 //     return num1 + num2             // explicit return
 // }
@@ -27,4 +28,4 @@ const addTwo = (num1, num2) => (num1 + num2); //implicit return (no need to writ
 const addTwos = (num1, num2) =>  { return num1 + num2}
 
 // console.log(addTwo(4, 5));
-// console.log(addTwo(3, 5));
+// console.log(addTwo(3, 5));  
