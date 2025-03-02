add7d06b8f02848978329f255a1f6716dee34638
Anurag Ganvir <144724285+anuragganvir@users.noreply.github.com>
2025-03-02 05:50:47 +0000

    commit

--- 03_basic/03Arrow.js
+++ 03_basic/03Arrow.js
@@ -26,7 +26,5 @@ const user = {
 const addTwo = (num1, num2) => (num1 + num2); //implicit return (no need to write return keyword)
 const addTwos = (num1, num2) =>  { return num1 + num2}
 
-
-
-console.log(addTwo(4, 5));
-console.log(addTwo(3, 5));
+// console.log(addTwo(4, 5));
+// console.log(addTwo(3, 5));
