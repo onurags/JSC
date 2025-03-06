6dd57c40a9abeb0cbeebabb394c08012866ea114
Anurag Ganvir <144724285+anuragganvir@users.noreply.github.com>
2025-03-06 15:05:13 +0000

    controlStructure

---
+++ 04_control_flow/one.js
@@ -0,0 +1,39 @@
+// if
+// const isUserloggedIn = true
+// const temperature = 41
+
+
+// if(temperature === 50){
+//     console.log("executed"); 
+// } else {
+//     console.log("not equal")
+// }
+
+
+
+// const score = 200
+
+// if(score > 100) {
+//     let power = "fly"
+//     console.log(`User power: ${power}`);
+// }
+
+// console.log(`${power}`);
+
+
+const balance = 1000
+// if(balance > 500) console.log("test")
+
+if (balance < 500) {
+    console.log("less than");
+    
+}else if (balance < 750) {
+    console.log("less than 750");
+    
+} else if (balance < 900){
+    console.log("less than 900");
+    
+} else {
+    console.log("greater than or equal to thousand");
+    
+}
